6dfb046a7526e249ce32825d896bc45b3d190e42
Anthony Bibbs <anthonybibbs@gmail.com>
2015-12-17 12:10:27 -0800

    Create socket.io server, initialize chat capability

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var socketServer = require('http').createServer(app);
+var io = require('socket.io')(socketServer);
+// io.on('connection', function(){ /* … */ });
+socketServer.listen(3000);
 
 app.use(express.static('../client/views'));
 console.log(__dirname);
@@ -8,9 +12,27 @@ app.get('/', function(req,res) {
 	res.render('index');
 });
 
-var server = app.listen((process.env.PORT || 3000), function() {
-  var host = server.address().address;
-  var port = server.address().port;
+socketServer.listen((process.env.PORT || 3000), function() {
+  var host = socketServer.address().address;
+  var port = socketServer.address().port;
 
   console.log('App launched and hosting at http://%s:%s',host,port);
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+// var server = app.listen((process.env.PORT || 3000), function() {
+//   var host = server.address().address;
+//   var port = server.address().port;
+
+//   console.log('App launched and hosting at http://%s:%s',host,port);
+// });

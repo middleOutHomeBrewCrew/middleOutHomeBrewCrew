1915c77356969aa22927fccb7c43b147094cee24
Michael Berber <mickberber@icloud.com>
2015-12-17 17:26:13 -0800

    fix merge conflict

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var socketServer = require('http').createServer(app);
+var io = require('socket.io')(socketServer);
+// io.on('connection', function(){ /* … */ });
+socketServer.listen(3000);
 
 app.use(express.static('../client'));
 
@@ -7,9 +11,28 @@ app.get('/', function(req,res) {
 	res.render('index');
 });
 
-var server = app.listen((process.env.PORT || 3000), function() {
-  var host = server.address().address;
-  var port = server.address().port;
+socketServer.listen((process.env.PORT || 3000), function() {
+  var host = socketServer.address().address;
+  var port = socketServer.address().port;
 
   console.log('App launched and hosting at http://%s:%s',host,port);
 });
+
+// create socket.io connection
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+// var server = app.listen((process.env.PORT || 3000), function() {
+//   var host = server.address().address;
+//   var port = server.address().port;
+
+//   console.log('App launched and hosting at http://%s:%s',host,port);
+// });

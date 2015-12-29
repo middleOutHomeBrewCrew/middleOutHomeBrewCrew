3a35e3253d246da3fe8fbd30ae3a7d7321728482
abibbs <anthonybibbs@gmail.com>
2015-12-29 02:03:27 -0800

    Implemented users feature

--- client/app/chat.js
+++ client/app/chat.js
@@ -1,30 +1,16 @@
-var socket = io();
 // chat Socket
+var socket = io();
+
+$('#chatForm').hide();
+$('#room').hide();
+$('#name').focus();
+$('#urlID').hide();
+
 //emit message to other sockets
 $('#chatForm').submit(function(){
-  socket.emit('chat message', $('#m').val());
-$('#m').val('');
-  return false;
-});
-//on event, add messages to chat box
-socket.on('chat message', function(msg){
-  $('#messages').prepend($('<li>').text(msg));
-});
-
-//url submit event, sends all sockets url to use
-$('#urlID').submit(function(){
-  socket.emit('url submit', $('#url').val());
-$('#url').val('');
-  return false;
-});
-
-// url Socket instantiate a youtube player for all
-//sockets to work on
-socket.on('url submit', function(url){
-  var player = new YT.Player('player', {
-    videoId : url.slice(32)
-  });
-  socket.player = player;
+	socket.emit('chat message', $('#m').val());
+	$('#m').val('');
+	return false;
 });
 
 //play video event
@@ -42,3 +28,67 @@ $('#pauseVid').on('click', function(){
 socket.on('pause video', function(){
   socket.player.pauseVideo();
 })
+
+// join the server upon submitting a username
+$('#join').click(function() {
+	var name = $('#name').val();
+	if (name != '') {
+		socket.emit('join', name);
+		ready = true;
+		$('#chatForm').show();
+		$('#room').show();
+		$('#m').focus();
+		$('#urlID').show();
+		$('#joinChat').hide();
+	}
+});
+
+$('#name').keypress(function(e) {
+	if (e.which === 13) {
+		var name = $('#name').val();
+		if (name != '') {
+			socket.emit('join',name);
+			ready = true;
+		}
+	}
+});
+
+// update notice informing local user of join (only shown to local user)
+socket.on('update', function(msg) {
+	if (ready) {
+		$('#messages').append($('<li>').text(msg));
+	}
+});
+
+// update notice informing remote user of join/leave (shown to all users)
+socket.on('update-people', function(people) {
+	if (ready) {
+		$('#people').empty();
+		$.each(people, function(clientid,name) {
+			$('#people').append($('<li>').text(name));
+		});
+	}
+});
+
+//on event, add messages to chat box
+socket.on('chat message', function(who,msg){
+	if (ready) {
+		$('#messages').append($('<li>').text(who + ': ' + msg));
+	}
+});
+
+//url submit event, sends all sockets url to use
+$('#urlID').submit(function() {
+	socket.emit('url submit', $('#url').val());
+	$('#url').val('');
+	return false;
+});
+
+// url Socket instantiate a youtube player for all
+//sockets to work on
+socket.on('url submit', function(url){
+	var player = new YT.Player('player', {
+    	videoId : url.slice(32);
+	});
+  socket.player = player;
+});
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var app = express();
 var socketServer = require('http').createServer(app);
 var io = require('socket.io')(socketServer);
 var path = require('path');
+var people = {};
 
 app.use(express.static(__dirname + './../client'));
 
@@ -22,6 +23,9 @@ app.get('/signup', function(req, res) {
     res.sendFile(path.join(__dirname + './../client/signup.html'));
 });
 
+
+app.use(express.static(__dirname + './../client'));
+
 socketServer.listen((process.env.PORT || 3000), function() {
   var host = socketServer.address().address;
   var port = socketServer.address().port;
@@ -30,25 +34,41 @@ socketServer.listen((process.env.PORT || 3000), function() {
 });
 
 // create socket.io connection
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+io.on('connection', function(socket) {
+
+  socket.on('join', function(name) {
+    people[socket.id] = name;
+    socket.emit('update','You have connected to the server');
+    io.emit('update', name + ' has joined the server.');
+    io.emit('update-people', people);
+    console.log(people[socket.id] + ' has connected!');
   });
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
+    io.emit('chat message', people[socket.id], msg);
+    console.log(people[socket.id] + ': ' + msg);
   });
+
+  socket.on('disconnect', function() {
+    io.emit('update', people[socket.id] + ' has left the server.');
+    delete people[socket.id];
+    io.emit('update-people',people);
+    console.log(people[socket.id] || socket.id + ' has disconnected!');
+  });
+
   socket.on('url submit', function(url){
     io.emit('url submit', url);
-    console.log('url: server', url);
+    console.log(people[socket.id] + ' has submitted a URL: ', url);
   });
+
   socket.on('play video', function(){
     io.emit('play video')
   });
+
   socket.on('pause video', function(){
     io.emit('pause video')
   });
+
 });
 
 

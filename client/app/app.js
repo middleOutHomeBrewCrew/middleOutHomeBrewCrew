57957136e5debea3f77cddcef9139eabb0a41952
rcardiff1 <robertstuartcardiff@gmail.com>
2016-01-06 18:52:36 -0800

    [update] edit for heroku deployment only

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,7 @@ function youtubeSearch(searchItem) {
     part : 'snippet',
     maxResults : 10,
     q: searchItem,
-    key: process.env.YOUTUBE_API_KEY },
+    key: 'AIzaSyCyU-0og7E6Sa1RpPr2OZrYKe2jYLGRuxI' },
     function(data) {
       $.each( data.items, function(i, item ) { 
         queryResults.push(item);

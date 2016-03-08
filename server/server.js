0ddba2e49180e07db77064292e2ee95b9be136bf
Michael Berber <mickberber@icloud.com>
2016-03-07 18:00:02 -0800

    [refactor] create socket connection functions

--- server/server.js
+++ server/server.js
@@ -2,9 +2,17 @@ var express = require('express');
 var app = express();
 var socketServer = require('http').createServer(app);
 var io = require('socket.io')(socketServer);
-var people = {};
 var routes = require('./routes.js');
+var ioConnection = require('./connection.js');
 
+socketServer.listen((process.env.PORT || 4000), function() {
+  var host = socketServer.address().address;
+  var port = socketServer.address().port;
+
+  console.log('App launched and hosting at http://%s:%s', host, port);
+});
+
+//application routes
 app.use(express.static(__dirname + './../client'));
 
 app.get('/', routes.index);
@@ -15,54 +23,26 @@ app.get('/login', routes.login);
 
 app.get('/signup', routes.signup);
 
-socketServer.listen((process.env.PORT || 4000), function() {
-  var host = socketServer.address().address;
-  var port = socketServer.address().port;
 
-  console.log('App launched and hosting at http://%s:%s',host,port);
-});
-
-// create socket.io connection
+// socket.io connections and functions
 io.on('connection', function(socket){
-  //start video socket
-  io.emit('new connection')
-  socket.on('new connection res', function(obj){
-    io.emit('new connection res', obj);
-  });
-  // register new user connect
-  socket.on('join', function(name) {
-    people[socket.id] = name;
-    socket.emit('update','You have connected to the server');
-    io.emit('update', name + ' has joined the server.');
-    io.emit('update-people', people);
-    console.log(people[socket.id] + ' has connected!');
-  });
-  
-  // log user message to chat
-  socket.on('chat message', function(msg){
-    io.emit('chat message', people[socket.id], msg);
-    console.log(people[socket.id] + ': ' + msg);
-  });
 
-  // log user disconnect to chat and update user list
-  socket.on('disconnect', function(){
-    var temp = people[socket.id];
-    
-    io.emit('update', temp + ' has left the server.');
-    delete people[socket.id];
-    io.emit('update-people',people);
-    console.log(temp + ' has disconnected!');
-  });
-  socket.on('url submit', function(url){
-    io.emit('url submit', url);
-    console.log(people[socket.id] + ' has submitted a URL: ', url);
-  });
-  socket.on('play video', function(){
-    io.emit('play video')
-  });
-  socket.on('pause video', function(){
-    io.emit('pause video')
-  });
+  ioConnection.newConnection(io);
+
+  ioConnection.ncresponse(io, socket);
+
+  ioConnection.join(io, socket);
+  
+  ioConnection.chatMessage(io, socket);
+
+  ioConnection.disconnect(io, socket);
+
+  ioConnection.urlSubmit(io, socket);
+  
+  ioConnection.playVideo(io, socket);
+  
+  ioConnection.pauseVideo(io, socket);
+
 });
 
 
